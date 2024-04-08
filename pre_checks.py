caf0903a1e7b50c7abf54922c8417ee953d78685
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-08 17:48:06 +0530

    Update pre_checks.py

--- pre_checks.py
+++ pre_checks.py
@@ -39,7 +39,7 @@ def check_configurations(config_data):
     return True
 
 def main():
-    folder_path = "pre-check/datamigration/configurations/raw/"  # Example folder path
+    folder_path = "pre-check/datamigration/configurations/raw"  # Example folder path
 
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
