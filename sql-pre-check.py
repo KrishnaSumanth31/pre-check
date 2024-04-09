e42600d749e129f6462ddb164fbf123e6f1e9be6
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-09 17:13:46 +0530

    Update sql-pre-check.py

--- sql-pre-check.py
+++ sql-pre-check.py
@@ -3,6 +3,12 @@ import os
 def validate_folder(folder_path):
     valid_files = []
     files = os.listdir(folder_path)
+    
+    # Check if there are any files in the directory
+    has_files = any(os.path.isfile(os.path.join(folder_path, f)) for f in files)
+    if not has_files:
+        return valid_files
+
     for file_name in files:
         if not file_name.endswith('.yaml'):
             print(f"Incorrect file extension in {folder_path}: {file_name}")
@@ -25,11 +31,10 @@ def validate_folder_structure(root_path):
     for root, dirs, files in os.walk(root_path):
         for folder in dirs:
             folder_path = os.path.join(root, folder)
-            if os.listdir(folder_path):  # Only validate folders with files
-                valid_files = validate_folder(folder_path)
-                for file_name in valid_files:
-                    file_path = os.path.join(folder_path, file_name)
-                    print(f"Valid file: {file_name} | Path: {file_path}")
+            valid_files = validate_folder(folder_path)
+            for file_name in valid_files:
+                file_path = os.path.join(folder_path, file_name)
+                print(f"Valid file: {file_name} | Path: {file_path}")
         
         # Check labtest subfolder if exists
         labtest_path = os.path.join(root, 'labtest')
