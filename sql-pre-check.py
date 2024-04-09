0d5de3c42d9431b79669a0f4b53c514db5a25b33
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-09 16:40:55 +0530

    Update sql-pre-check.py

--- sql-pre-check.py
+++ sql-pre-check.py
@@ -4,25 +4,26 @@ def validate_folder(folder_path):
     valid_files = []
     files = os.listdir(folder_path)
     for file_name in files:
-        if file_name.endswith('.yaml'):
-            if folder_path.endswith('stg') or folder_path.endswith('trn'):
-                if file_name.startswith('insert_script'):
-                    valid_files.append(os.path.join(folder_path, file_name))
-            elif folder_path.endswith('ext'):
-                if file_name.startswith('ext_script'):
-                    valid_files.append(os.path.join(folder_path, file_name))
+        if not file_name.endswith('.yaml'):
+            continue
+        
+        if folder_path.endswith('stg') or folder_path.endswith('trn'):
+            if file_name.startswith('insert_script'):
+                valid_files.append((file_name, os.path.join(folder_path, file_name)))
+        elif folder_path.endswith('ext'):
+            if file_name.startswith('ext_script'):
+                valid_files.append((file_name, os.path.join(folder_path, file_name)))
+
     return valid_files
 
 def validate_folder_structure(root_path):
-    valid_files = []
     for root, dirs, files in os.walk(root_path):
         for folder in dirs:
             folder_path = os.path.join(root, folder)
-            valid_files.extend(validate_folder(folder_path))
-    return valid_files
+            valid_files = validate_folder(folder_path)
+            for file_name, file_path in valid_files:
+                print(f"Valid file: {file_name} | Path: {file_path}")
 
 # Example usage:
-root_path = "datamigration/sql"
-valid_files = validate_folder_structure(root_path)
-for file_path in valid_files:
-    print("Valid file path:", file_path)
+root_path = "artifacts-dev/sql"
+validate_folder_structure(root_path)
