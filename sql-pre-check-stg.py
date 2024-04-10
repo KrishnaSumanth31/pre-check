c6b811444106f90c6f122aff5b6358aea70dd31a
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-10 12:54:56 +0530

    Update sql-pre-check-stg.py

--- sql-pre-check-stg.py
+++ sql-pre-check-stg.py
@@ -4,21 +4,27 @@ import yaml
 
 def check_yaml_files(folder_paths):
     for folder_path in folder_paths:
-        for file_path in glob.glob(os.path.join(folder_path, "insert_script*.yaml")) + glob.glob(os.path.join(folder_path, "insert_script*.yml")):
+        yaml_files = glob.glob(os.path.join(folder_path, "insert_script*.yaml")) + glob.glob(os.path.join(folder_path, "insert_script*.yml"))
+        for file_path in yaml_files:
             try:
                 with open(file_path, 'r') as yaml_file:
                     yaml_data = yaml.safe_load(yaml_file)
-                    if isinstance(yaml_data, list):
-                        check_sequence(yaml_data, file_path)
+                    if isinstance(yaml_data, dict):
+                        executes = yaml_data.get('executes', [])
+                        if isinstance(executes, list):
+                            check_sequence(executes, file_path)
+                        else:
+                            print(f"Invalid YAML format in file: {file_path}")
                     else:
                         print(f"Invalid YAML format in file: {file_path}")
-            except yaml.YAMLError as e:
-                print(f"Error reading YAML file {file_path}: {e}")
+            except Exception as e:
+                print(f"Error processing YAML file {file_path}: {e}")
 
-def check_sequence(yaml_data, file_path):
-    sql_commands = [item.get('sql', '').strip().lower() for item in yaml_data if 'sql' in item]
+def check_sequence(executes, file_path):
     expected_sequence = ['delete', 'commit', 'insert', 'commit']
-    if sql_commands == expected_sequence:
+    actual_sequence = [item.get('sql', '').strip().lower() for item in executes]
+    
+    if actual_sequence == expected_sequence:
         print(f"Sequence is correct in file: {file_path}")
     else:
         print(f"Sequence is incorrect in file: {file_path}")
