32fc22d6d411935b8a795ec6160da568e4996b17
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-10 11:45:52 +0530

    Update sql-pre-check-stg.py

--- sql-pre-check-stg.py
+++ sql-pre-check-stg.py
@@ -4,15 +4,19 @@ import yaml
 def check_file(filename):
     with open(filename, 'r') as file:
         data = yaml.safe_load(file)
-        keys = list(data.keys())
         expected_keys = ['delete', 'commit', 'insert', 'commit']
-        if keys == expected_keys:
+        actual_keys = list(data[0].keys()) if isinstance(data, list) and len(data) > 0 else []
+        if actual_keys == expected_keys:
             return True
         else:
             return False
 
 def perform_checks(directory):
     for root, dirs, files in os.walk(directory):
+        # Skip the 'sql/ext' folder
+        if root.endswith('ext'):
+            continue
+        # Perform checks for files in 'sql/stg' and 'sql/trn' folders
         for file in files:
             if file.startswith('insert_script') and file.endswith('.yaml'):
                 filepath = os.path.join(root, file)
