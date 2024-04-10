b9a89b6844fe038a90dd94cac14882dda16ea3bf
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-10 11:38:52 +0530

    Update sql-pre-check-stg.py

--- sql-pre-check-stg.py
+++ sql-pre-check-stg.py
@@ -5,21 +5,22 @@ def check_file(filename):
     with open(filename, 'r') as file:
         data = yaml.safe_load(file)
         keys = list(data.keys())
-        if keys == ['delete', 'commit', 'insert', 'commit']:
-            return True, keys
+        expected_keys = ['delete', 'commit', 'insert', 'commit']
+        if keys == expected_keys:
+            return True
         else:
-            return False, None
+            return False
 
 def perform_checks(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.startswith('insert_script') and file.endswith('.yaml'):
                 filepath = os.path.join(root, file)
-                passed, sequence = check_file(filepath)
+                passed = check_file(filepath)
                 if passed:
-                    print(f"File: {filepath}, Sequence: {sequence}, Status: Passed pre-checks.")
+                    print(f"File: {filepath}, Status: Passed pre-checks.")
                 else:
-                    print(f"File: {filepath}, Sequence: None, Status: Failed pre-checks.")
+                    print(f"File: {filepath}, Status: Failed pre-checks.")
 
 # Specify the directory to perform the checks
 directory = 'datamigration/sql'
