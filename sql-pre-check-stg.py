98702cceec772ea1dde00bc2651bb925db3c92fa
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-10 13:09:27 +0530

    Update sql-pre-check-stg.py

--- sql-pre-check-stg.py
+++ sql-pre-check-stg.py
@@ -4,21 +4,27 @@ import yaml
 
 def check_yaml_files(folder_paths):
     for folder_path in folder_paths:
+        # Find YAML files matching the criteria
         yaml_files = glob.glob(os.path.join(folder_path, "insert_script*.yaml")) + glob.glob(os.path.join(folder_path, "insert_script*.yml"))
         for file_path in yaml_files:
-            try:
-                with open(file_path, 'r') as yaml_file:
-                    yaml_data = yaml.safe_load(yaml_file)
-                    if isinstance(yaml_data, dict):
-                        executes = yaml_data.get('executes', [])
-                        if isinstance(executes, list):
-                            check_sequence(executes, file_path)
+            # Check if file name starts with "insert_script" and extension is ".yaml" or ".yml"
+            if os.path.basename(file_path).startswith("insert_script") and file_path.lower().endswith(('.yaml', '.yml')):
+                print(f"Valid file: {file_path}")
+                try:
+                    with open(file_path, 'r') as yaml_file:
+                        yaml_data = yaml.safe_load(yaml_file)
+                        if isinstance(yaml_data, dict):
+                            executes = yaml_data.get('executes', [])
+                            if isinstance(executes, list):
+                                check_sequence(executes, file_path)
+                            else:
+                                print(f"Invalid YAML format in file: {file_path}")
                         else:
                             print(f"Invalid YAML format in file: {file_path}")
-                    else:
-                        print(f"Invalid YAML format in file: {file_path}")
-            except Exception as e:
-                print(f"Error processing YAML file {file_path}: {e}")
+                except Exception as e:
+                    print(f"Error processing YAML file {file_path}: {e}")
+            else:
+                print(f"Ignored file: {file_path}. File name does not start with 'insert_script' or has invalid extension.")
 
 def check_sequence(executes, file_path):
     expected_sequence = ['delete', 'commit', 'insert', 'commit']
