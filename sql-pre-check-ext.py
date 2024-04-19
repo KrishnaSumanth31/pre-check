aeb5e5e50205ba9597c2ba4cafee648211163c1d
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-19 19:14:02 +0530

    Update sql-pre-check-ext.py

--- sql-pre-check-ext.py
+++ sql-pre-check-ext.py
@@ -1,26 +1,33 @@
 import os
 import re
 
+def has_sql_command(content):
+    # Check if the content contains any SQL command
+    sql_commands = re.findall(r'\b(SELECT|INSERT|UPDATE|DELETE)\b', content, re.IGNORECASE)
+    return bool(sql_commands)
+
+def has_trn_schema(content):
+    # Check if the content contains the $trn_schema key
+    return bool(re.search(r'\$trn_schema', content))
+
 def validate_yaml_file(file_path):
     # Check if file name starts with 'ext_script' and has extension .yml or .yaml
     if re.match(r'^ext_script.*\.(yml|yaml)$', os.path.basename(file_path)):
-        # Check if the file contains at least one SQL command with $raw_schema
+        # Check if the file contains at least one SQL command and $trn_schema
         with open(file_path, 'r') as file:
             content = file.read()
-            if re.search(r'\b(SELECT|INSERT|UPDATE|DELETE)\b.*\$raw_schema', content, re.IGNORECASE):
-                return True
-    return False
+            return has_sql_command(content), has_trn_schema(content)
+    return False, False
 
 def main():
     directory = 'datamigration/sql/ext'  # Path to the directory
     for file_name in os.listdir(directory):
         file_path = os.path.join(directory, file_name)
         if os.path.isfile(file_path):
-            is_valid = validate_yaml_file(file_path)
-            if is_valid:
-                print(f"File: {file_name} | Path: {file_path} | Valid SQL Command with $raw_schema: Yes")
-            else:
-                print(f"File: {file_name} | Path: {file_path} | Valid SQL Command with $raw_schema: No")
+            has_sql, has_trn = validate_yaml_file(file_path)
+            sql_command_msg = "SQL Command: Yes" if has_sql else "SQL Command: No"
+            trn_schema_msg = "$trn_schema: Yes" if has_trn else "$trn_schema: No"
+            print(f"File: {file_name} | Path: {file_path} | {sql_command_msg} | {trn_schema_msg}")
 
 if __name__ == "__main__":
     main()
