acb9755520818f97ed71837c3e41a6b42f6c6fe7
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-23 15:00:30 +0530

    Update pre-check-for-python-files.py

--- pre-check-for-python-files.py
+++ pre-check-for-python-files.py
@@ -11,12 +11,16 @@ def check_file_extension(filename):
 
 def check_for_secrets(file_path):
     """Check if any secrets are hardcoded."""
+    excluded_words = ['password', 'postgre', 'schema', 'username']
     with open(file_path, 'r') as file:
         lines = file.readlines()
         for line_number, line in enumerate(lines, start=1):
+            # Exclude specific words from the check
+            if any(word in line.lower() for word in excluded_words):
+                continue
             # Example check for 'password' keyword
             if 'password' in line.lower():
-                print(f"Hardcoded value found in file: {file_path}, line {line_number}: {line.strip()}")
+                print(f"ERROR: Hardcoded value found in file: {file_path}, line {line_number}: {line.strip()}")
 
 def perform_pre_checks(directory):
     """Perform pre-checks on Python files within the specified directory."""
@@ -25,10 +29,10 @@ def perform_pre_checks(directory):
             if file.endswith('.py'):
                 file_path = os.path.join(root, file)
                 if check_file_name(file) and check_file_extension(file):
-                    print(f"Valid file: {file_path}")
+                    print(f"INFO: Valid file: {file_path}")
                     check_for_secrets(file_path)
                 else:
-                    print(f"Invalid file: {file_path}")
+                    print(f"ERROR: Invalid file: {file_path}")
 
 # Usage
 directory = 'datamigration/glue/scripts'
